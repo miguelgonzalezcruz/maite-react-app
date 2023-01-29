1493e66e330d8ab32b8ad1029fd0c60762801498
Miguel Gonzalez <miguel.glez.cruz@gmail.com>
2023-01-29 11:40:28 +0100

    header&hero desktop

---
+++ src/components/Header.js
@@ -0,0 +1,21 @@
+import React from "react";
+import "../blocks/Header.css";
+import logo from "../images/logo-maitereciclando.svg";
+
+function Header() {
+  return (
+    <header className="header">
+      <div className="header__container">
+        <img className="header__logo" src={logo} alt="Logo Maite Reciclando" />
+
+        <div className="navigation__container">
+          <button className="navigation__button-signup">Sign up</button>
+          <button className="navigation__button-login">Log in</button>
+        </div>
+      </div>
+      <div className="header__divider"></div>
+    </header>
+  );
+}
+
+export default Header;
