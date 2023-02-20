e9225bdad9ee64b8222891654ddb9894ad9495ad
Miguel Gonzalez <miguel.glez.cruz@gmail.com>
2023-02-20 21:07:48 +0100

    minor changes

--- src/components/RegisterModal.js
+++ src/components/RegisterModal.js
@@ -80,7 +80,7 @@ const RegisterModal = ({
         type="password"
         name="password"
         value={password}
-        placeholder="Enter password"
+        placeholder="minlength: 8"
         onChange={handlePassword}
         required
       />
@@ -110,7 +110,7 @@ const RegisterModal = ({
         type="tel"
         name="phone"
         value={phone}
-        placeholder="Enter phone"
+        placeholder="minlength: 5, maxlength: 20"
         onChange={handlePhone}
         required
       />
