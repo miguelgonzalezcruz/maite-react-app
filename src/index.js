ea8b8714261b179dd12c3cc141e0d0e472668810
Miguel Gonzalez <miguel.glez.cruz@gmail.com>
2023-03-12 01:27:21 +0100

    tagmanager

--- src/index.js
+++ src/index.js
@@ -6,6 +6,14 @@ import reportWebVitals from "./reportWebVitals";
 
 import "./index.css";
 
+import TagManager from "react-gtm-module";
+
+const tagManagerArgs = {
+  gtmId: "GTM-55MLR5Q",
+};
+
+TagManager.initialize(tagManagerArgs);
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
